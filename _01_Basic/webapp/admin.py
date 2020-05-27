831ce08e40da177fafa81815f9e80adcba9d39c4
Duy Nguyen <nguyenvunhatduy@gmail.com>
2020-05-27 23:55:25 +0800

    _01_Basic: Completed

--- _01_Basic/webapp/admin.py
+++ _01_Basic/webapp/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from . models import employees
 
 from django.contrib import admin
 
+admin.site.register(employees)
 # Register your models here.
--- _01_Basic/webapp/models.py
+++ _01_Basic/webapp/models.py
@@ -3,8 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
 class employees(models.Model):
-    firstname=models.CharField(max_length=10)
-    lastname=models.CharField(max_length=10)
-    emp_id=models.IntegerField
+    firstname = models.CharField(max_length=10)
+    lastname = models.CharField(max_length=10)
+    emp_id = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.firstname
