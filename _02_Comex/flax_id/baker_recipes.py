78b3ad6e206cbd6126b2a046a9e226fd0508607c
Duy Nguyen <nguyenvunhatduy@gmail.com>
2020-12-28 11:59:52 +0700

    added demo selenium

--- _02_Comex/flax_id/baker_recipes.py
+++
@@ -1,4 +0,0 @@
-from model_bakery import baker
-from flax_id import get_flax_id
-
-baker.generators.add('flax_id.django.fields.FlaxId', get_flax_id)
---
+++ _03_SeleniumDemo/main.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+PATH = "/Users/duynguyen/Downloads/chromedriver"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://nhatduy.info")
